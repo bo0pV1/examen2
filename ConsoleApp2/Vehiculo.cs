021dc67d2e924c3241d98bcc63ccb171447002d1
INFO-4 <INFO-4@DESKTOP-8L4NFHR>
2025-12-10 21:23:43 -0300

    Agregar archivos de proyecto.

---
+++ ConsoleApp2/Vehiculo.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    public abstract class Vehiculo
+    {
+       public int id { get; set; }
+        public string dueño {get; set;}
+
+        public abstract void Precio(int Tarifa);
+        
+    }
+}
